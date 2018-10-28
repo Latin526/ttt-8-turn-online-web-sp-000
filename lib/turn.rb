e36ff9190f4d9ba198dd537b72bf75faba45feea
Yasmin Torres <yasmintorres90@hotmail.com>
2018-10-28 00:09:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,29 +1,39 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+ def display_board(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
- def valid_move?(board, position)
-  if position_taken?(board, position.to_i-1) || !(position.to_i-1).between?(0,8)
-    false
-  else
+ def input_to_index(input)
+  input.to_i-1
+end
+ def valid_move(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
     true
+  else 
+    false
   end
 end
- def position_taken?(board, position)
-  board[position] == "X" || board[position] == "O"
+ def position_taken?(board, index)
+  if board[index] == "x" || board[index] == "o"
+    true
+  else
+    false
+  end
 end
- def move(board, position, player = "X")
-  board[position.to_i-1] = player
+ def move(board, position, charatcer = "x")
+  board[position] = charatcer
 end
  def turn(board)
-  puts "Please enter 1-9:"
-  if valid_move?(board, input = gets.strip)
-    move(board, input)
-    display_board(board)
+  puts "Please enter 1-9."
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+  move(board, index)
+  display_board(board)
   else
-    turn(board)
+  turn(board)
   end
 end 
